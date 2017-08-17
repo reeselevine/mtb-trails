d9150f3a6ffaf9006df6d61e0851d24aad010b69
reeselevine <reeselevine1@gmail.com>
2017-08-16 21:07:30 -0700

    Write trail search function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var Alexa = require('alexa-sdk');
-var http = require('http');
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'Google Maps API key here'
+});
+var https = require('https');
 
 var states = {
     SEARCHMODE: '_SEARCHMODE',
@@ -12,7 +15,13 @@ var welcomeRemprompt = "Ask me for trails near any city";
 
 var helpMessage = "Here is something you can ask me: Find me trails near Boulder, Colorado. After that, you can say: give me more information about trail number two. You can also say: Tell me about a place with cool trails. What do you want to do?";
 
-var goodbyeMessage = "Thanks for using MTB trails. See you on the mountain!";
+var goodbyeMessage = "Thanks for using MTB trails. See you out on the bike!";
+
+var noCityErrorMessage = "Sorry, I didn't recognize that city name. Please try again.";
+
+var cityConversionErrorMessage = "Something went wrong while looking for trails. Please try again.";
+
+var numberOfTrails = 3;
 
 var ouput = "";
 
@@ -20,6 +29,7 @@ var alexa;
 
 var newSessionHandlers = {
   'LaunchRequest': function () {
+      this.handler.state = states.SEARCHMODE;
       this.emit(':ask', welcomeMessage, welcomeReprompt);
   },
   'AMAZON.StopIntent': function () {
@@ -37,6 +47,64 @@ var newSessionHandlers = {
 };
 
 var searchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
+  'getTrailsIntent': function () {
+      var city = '';
+      var lat = 0;
+      var lng = 0;
+      if (this.event.request.slots.city) {
+          if (this.event.request.intent.slots.city.value) {
+              city = this.event.request.intent.slots.city.value;
+          }
+      }
+
+      if (city) {
+        googleMapsClient.geocode({
+          address: city
+        }, function(err, response) {
+          if (!err) {
+            lat = response.json.results.geometry.location.lat; 
+            lng = response.json.results.geometry.location.lng;
+          }
+        });
+
+        if (lat != 0 && lng != 0) {
+          var loc = [lat, lng];
+          httpsGet(loc, function (response) {
+            var responseData = JSON.parse(response);
+            var cardContent = "Data provided by MTB Project\n\n";
+
+            if (responseData == null) {
+              output = "There was a problem getting trail data. Please try again.";
+            } else {
+              output = "There are " + responseData.trails.length + " trails near " + city + ".";
+              if (responseData.trails.length > numberOfTrails) {
+                output += " Here are the top " + numberOfTrails + ".";
+              }
+              output += " See your Alexa app for more information and trails.";
+              
+              for (var i = 0; i < responseData.trails.length; i++) {
+                if (i < numberOfTrails) {
+                  var name = responseData.trails[i].name;
+                  var summary = responseData.trails[i].summary;
+                  var difficulty = responseData.trails[i].difficulty;
+
+                  output += name + ". " + summary + " Difficulty: " + difficulty + ";";
+                }
+                cardContent += name + "\n" + summary + "\n" + "Difficulty: " + difficulty + "\n\n"; 
+              }
+              
+            }
+            var cardTitle = city + " Trails";
+            alexa.emit(':tellWithCard', output, cardTitle, cardContent);
+          } 
+        } else {
+          this.emit(':ask', cityConversionErrorMessage);
+        }
+      } else {
+        this.emit(':ask', noCityErrorMessage);
+      }
+  },
+
   'AMAZON.YesIntent': function () {
       this.emit(':ask', helpMessage, helpMessage);
   },
@@ -89,6 +157,28 @@ var trailInfoHandlers = Alexa.CreateStateHandler(states.TRAILINFO, {
       this.emit(':ask', helpMessage, welcomeRemprompt);
   }
 });
+
+function httpsGet(query, callback) {
+  var options = {
+    host: 'https://www.mtbproject.com',
+    path: '/data/get-trails?lat=' + query[0] + '&lon=' + query[1] + '&key=' + "MTB Project API key here",
+    method: 'GET'
+ 
+    var req = https.request(options, (res) => {
+      var body = '';
+      res.on('data', (d) => {
+        body += d;
+      });
+      res.on('end', function () {
+        callback(body);
+      });
+    });
+    req.end();
+    req.on('error', (e) => {
+      console.error(e);
+    });
+  };
+}
 
 
 
